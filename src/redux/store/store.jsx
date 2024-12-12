72a3ab00db7f7c96e79460bf535c37782ca026a5
Sibusiso Matebese <sibusisomatebese75@gmail.com>
2024-12-12 15:08:37 +0200

    fix

--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -4,7 +4,6 @@ import articleReducer from "../slices/articleSlice"
 import jobReducer from "../slices/jobSlice"
 import reportReducer from "../slices/reportSlice"
 import savedJobsReducer from "../slices/savedJobsSlice"
-import employerReducer from "../slices/employerslice";
 import feedbackReducer from "../slices/feedbackSlice";
 import skillReducer from "../slices/skillSlice";
 
@@ -12,7 +11,6 @@ import skillReducer from "../slices/skillSlice";
 const store = configureStore({
     reducer:{
        users:userReducer,
-       employers:employerReducer,
        articles:articleReducer,
        jobs:jobReducer,
        reports:reportReducer,
