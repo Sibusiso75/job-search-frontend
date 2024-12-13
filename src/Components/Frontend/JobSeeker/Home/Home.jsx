bfc5d917327dd3d534c4fdb2e9cb08d972ff9e6e
Sibusiso Matebese <sibusisomatebese75@gmail.com>
2024-12-13 08:51:37 +0200

    l

--- src/Components/Frontend/JobSeeker/Home/Home.jsx
+++ src/Components/Frontend/JobSeeker/Home/Home.jsx
@@ -16,7 +16,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
-
+import MyLoader from '../../../../MyLoader'
 function Home() {
   
   let navigate = useNavigate()
